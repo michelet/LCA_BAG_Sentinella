7e329017aa330bb9bcde7e7bda55d52f94060444
michelet <cedric.michelet@gmail.com>
2015-10-29 21:13:06 +0100

    Change namespace.

--- LCA_BAG_Sentinella_Server/src/ch/bfh/lca/_15h/server/LCA_BAG_Sentinella_Server.java
+++ LCA_BAG_Sentinella_Server/src/ch/bfh/lca/_15h/server/LCA_BAG_Sentinella_Server.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lca_bag_sentinella_server;
+package ch.bfh.lca._15h.server;
 
 /**
  *
