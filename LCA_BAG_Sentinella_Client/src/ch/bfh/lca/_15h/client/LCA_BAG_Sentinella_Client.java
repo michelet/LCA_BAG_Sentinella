1e18542fef17b575262e48ea4237d451a784abd5
Stefan <Stefan@NBLENOVO>
2015-10-29 21:35:29 +0100

    Testing Git

--- LCA_BAG_Sentinella_Client/src/ch/bfh/lca/_15h/client/LCA_BAG_Sentinella_Client.java
+++ LCA_BAG_Sentinella_Client/src/ch/bfh/lca/_15h/client/LCA_BAG_Sentinella_Client.java
@@ -16,6 +16,7 @@ public class LCA_BAG_Sentinella_Client {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Bonjour monde");
     }
     
 }
