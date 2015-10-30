b348c2dcdf34e1687a69a133171da985632206eb
michelet <cedric.michelet@gmail.com>
2015-10-30 14:44:36 +0100

    Add comments.

--- LCA_BAG_Sentinella_Library/src/ch/bfh/lca/_15h/library/DataSource.java
+++ LCA_BAG_Sentinella_Library/src/ch/bfh/lca/_15h/library/DataSource.java
@@ -8,10 +8,22 @@ package ch.bfh.lca._15h.library;
 import ch.bfh.lca._15h.library.model.Patient;
 
 /**
- *
- * @author micheletc
+ * Interface for all classes which can read data from a source.
+ * @author Cédric Michelet
  */
 public interface DataSource {
+    /**
+     * Count the number of patients available in the source
+     * @return Number of patients available in the source
+     * @throws Exception 
+     */
     int countPatients() throws Exception;
+    
+    /**
+     * Return a specific patient
+     * @param index Position of the patient in the list
+     * @return Required patient
+     * @throws Exception 
+     */
     public Patient getPatient(int index) throws Exception;
 }
