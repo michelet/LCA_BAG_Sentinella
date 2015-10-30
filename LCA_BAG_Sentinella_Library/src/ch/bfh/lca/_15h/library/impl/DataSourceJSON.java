1531323b97833baed62ede68a6d7d136e374329c
michelet <cedric.michelet@gmail.com>
2015-10-30 08:40:55 +0100

    Add class DataSourceJSON

---
+++ LCA_BAG_Sentinella_Library/src/ch/bfh/lca/_15h/library/impl/DataSourceJSON.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.bfh.lca._15h.library.impl;
+
+import ch.bfh.lca._15h.library.DataSource;
+import ch.bfh.lca._15h.library.model.Activity;
+import ch.bfh.lca._15h.library.model.Patient;
+import java.util.List;
+
+/**
+ *
+ * @author micheletc
+ */
+public class DataSourceJSON implements DataSource {
+
+    @Override
+    public List<Patient> getPatientsList() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Activity> getActivitiesList() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
