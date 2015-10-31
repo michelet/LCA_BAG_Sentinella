bac4a60fe7044b771b05761956fc2533f7cf3da6
michelet <cedric.michelet@gmail.com>
2015-10-31 14:43:48 +0100

    Add interface ActivityFilter

---
+++ LCA_BAG_Sentinella_Library/src/ch/bfh/lca/_15h/library/ActivityFilter.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.bfh.lca._15h.library;
+
+import ch.bfh.lca._15h.library.model.Activity;
+
+/**
+ * Class to filter activity according to internal conditions.
+ * @author Cédric Michelet
+ */
+public interface ActivityFilter {
+    public Boolean matchActivity(Activity activity);    
+}
