527364b1a5dbe601d3707ab0983525a610f46156
michelet <cedric.michelet@gmail.com>
2015-10-31 14:44:12 +0100

    Add class ActivityFilterOnDate

---
+++ LCA_BAG_Sentinella_Library/src/ch/bfh/lca/_15h/library/impl/ActivityFilterOnDate.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.bfh.lca._15h.library.impl;
+
+import ch.bfh.lca._15h.library.ActivityFilter;
+import ch.bfh.lca._15h.library.model.Activity;
+import java.util.Date;
+
+/**
+ * Filter activities based on their date
+ * @author Cédric Michelet
+ */
+public class ActivityFilterOnDate implements ActivityFilter {
+    private Date startDate = null;
+    private Date endDate = null;
+    
+    public ActivityFilterOnDate(Date startDateIncluded, Date endDateIncluded) {
+        startDate = startDateIncluded;
+        endDate = endDateIncluded;
+    }
+    
+    @Override
+    public Boolean matchActivity(Activity activity) {
+        return true;
+    }   
+}
