bc4b8779085d5cb01ee58185da9e61489a5ebe21
Stefan <Stefan@NBLENOVO>
2015-11-06 17:28:52 +0100

    Created new abstract Database class. Added the SQLi Database Object.

--- LCA_BAG_Sentinella_Library/src/ch/bfh/lca/_15h/library/Database/MSAccessDatabase.java
+++ LCA_BAG_Sentinella_Library/src/ch/bfh/lca/_15h/library/Database/MSAccessDatabase.java
@@ -1,77 +1,32 @@
 package ch.bfh.lca._15h.library.Database;
 
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-public class MSAccessDatabase implements IDatabase {
+public class MSAccessDatabase extends AbstractDatabase implements IDatabase {
 
+    
+    private final String DRIVERCLASS = "jdbc:ucanaccess://";
+    
     private final String filePath;
-    private final String connectionString;
-    Connection connection;
 
     
     public MSAccessDatabase(URL fileURL) {
         this.filePath = fileURL.getPath();
-        this.connectionString = "jdbc:ucanaccess://" + filePath;
+        this.connectionString = DRIVERCLASS + this.filePath;
     }
 
      public MSAccessDatabase(String filePath) {
         this.filePath = filePath;
-        this.connectionString = "jdbc:ucanaccess://" + this.filePath;
-    }
-    
-    
-    @Override
-    public Connection getConnection() {
-        if (this.connection == null) {
-            return null;
-        } else {
-            return this.connection;            
-        }
+        this.connectionString = DRIVERCLASS + this.filePath;
     }
 
     @Override
-    public void closeConnection() throws SQLException {
-        if (this.getConnection() != null) {
-            if (!this.getConnection().isClosed()) {
-                this.getConnection().close();
-                this.destroyConnection();
-            }
-        }
+    public String getConnectionString() {
+        return this.connectionString;
     }
 
     @Override
-    public void openConnection()  throws SQLException  {
-        connection=DriverManager.getConnection(this.connectionString);
+    public void setConnectionString(String connectionString) {
+        this.connectionString = connectionString;
     }
-
-    @Override
-    public boolean hasOpenConnection() {
-        
-        boolean isOpen = false;
-        try {
-          if (this.getConnection() != null) {
-            if (!this.getConnection().isClosed()) {
-                isOpen = true;
-            }
-          }   
-          return isOpen;
-        } catch (SQLException ex) {
-            Logger.getLogger(MSAccessDatabase.class.getName()).log(Level.SEVERE, null, ex);
-            this.destroyConnection();
-            return false;
-        } 
-    }
-    
-    /**
-     * Destroys the Connection Object which is stored in the connection variable
-     */
-    private void destroyConnection() {
-        this.connection = null;
-    }
-
 }
