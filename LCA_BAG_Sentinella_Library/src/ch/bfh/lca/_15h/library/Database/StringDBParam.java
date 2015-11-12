9d4220f7f08da5f7709b13033fb5637fa1048050
Stefan <Stefan@NBLENOVO>
2015-11-12 21:14:37 +0100

    Added LongDBParam and StringDBParam

---
+++ LCA_BAG_Sentinella_Library/src/ch/bfh/lca/_15h/library/Database/StringDBParam.java
@@ -0,0 +1,31 @@
+package ch.bfh.lca._15h.library.Database;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * IntegerDBParam has to add integer parameters to a prepared sql statmenet. 
+ * @author Stefan Schöpfer
+ * @see ch.bfh.lca._15h.library.Database.ADBParameter
+ * @see ch.bfh.lca._15h.library.Database.IDBParameter
+ */
+public class StringDBParam extends ADBParameter<String> implements IDBParameter {
+
+    /**
+     * Constructor method. Sets the Index of the paramter (which starts with 1) and the value as integer.
+     * @param index - int (Starts at 1)
+     * @param value - Integer
+     */
+    public StringDBParam(int index, String value) {
+        super(index, value);
+    }
+
+    @Override
+    public PreparedStatement addParamToStatement(PreparedStatement statement) throws SQLException{
+        PreparedStatement resultStatement = statement;
+        
+        resultStatement.setString(this.getIndex(), this.getValue());
+        
+        return resultStatement;
+    }
+}
