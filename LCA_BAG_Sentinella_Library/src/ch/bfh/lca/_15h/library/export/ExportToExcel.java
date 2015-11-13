13d17c9f03f543768a58a10bf9ebfb0ecbd5e019
michelet <cedric.michelet@gmail.com>
2015-11-13 15:26:45 +0100

    Add support for XLSX.

--- LCA_BAG_Sentinella_Library/src/ch/bfh/lca/_15h/library/export/ExportToExcel.java
+++ LCA_BAG_Sentinella_Library/src/ch/bfh/lca/_15h/library/export/ExportToExcel.java
@@ -12,9 +12,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 
 /**
  *
@@ -22,38 +25,54 @@ import org.apache.poi.ss.usermodel.Workbook;
  */
 public class ExportToExcel {
 
-    public static void exportToExcel(Translation.TRANSLATION_LANGUAGE language, IResultRow[] rows, String excelFilePath) throws FileNotFoundException, IOException {
-        Workbook wb = new HSSFWorkbook();
-
+    public static void exportToExcel(Translation.TRANSLATION_LANGUAGE language, String sheetTitle, String tableTitle, IResultRow[] rows, String excelFilePath) throws FileNotFoundException, IOException {
+        //Workbook wb = new HSSFWorkbook(); //xls
+        Workbook wb = new SXSSFWorkbook(); //xlsx
         //create new sheet
-        Sheet sheet1 = wb.createSheet("Sentinella");
+        Sheet sheet1 = wb.createSheet(sheetTitle);
         Row row;
         Cell cell;
         String translation;
-        
-        for(int i=0; i<rows.length; i++) {
+
+        int rowIndex = 0;
+
+        //add title
+        row = sheet1.createRow((short) rowIndex);
+        cell = row.createCell(0);
+        cell.setCellValue(tableTitle);
+        CellStyle style = wb.createCellStyle();
+        Font font = wb.createFont();
+        font.setFontHeightInPoints((short) 24);
+        //font.setFontName("Courier New");
+        style.setFont(font);
+        cell.setCellStyle(style);
+
+        //add rows
+        for (int i = 0; i < rows.length; i++) {
             //if first line, write col names
-            if(i==0) {
-                row = sheet1.createRow((short)0);
-                
-                for(int j=0; j<rows[i].getColNames().length; j++) {
+            if (i == 0) {
+                row = sheet1.createRow((short) rowIndex + 2);
+
+                for (int j = 0; j < rows[i].getColNames().length; j++) {
                     cell = row.createCell(j);
-                    
+
                     //look for translation
                     translation = Translation.getForKey(language, rows[i].getColNames()[j]);
-                    if(translation == null) translation = rows[i].getColNames()[j]; //if doesn't found a translation for the column take name of col
+                    if (translation == null) {
+                        translation = rows[i].getColNames()[j]; //if doesn't found a translation for the column take name of col
+                    }
                     cell.setCellValue(translation);
                 }
             }
-            
-            row = sheet1.createRow((short)(i+1));
-            
-            for(int j=0; j<rows[i].getColNames().length; j++) {
+
+            row = sheet1.createRow((short) (rowIndex + i + 3));
+
+            for (int j = 0; j < rows[i].getColNames().length; j++) {
                 cell = row.createCell(j);
                 cell.setCellValue(rows[i].getValueAt(j).toString());
             }
         }
-        
+
         //write to the file
         FileOutputStream fileOut = new FileOutputStream(excelFilePath);
         wb.write(fileOut);
